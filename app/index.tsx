962ed7fe70c818ffcbc467d4cd955f3a53701802
ooliveiratg <menezesth16@gmail.com>
2026-02-12 10:51:05 -0300

    feat(splash-screen): implement animated splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useRef } from "react";
-import { StatusBar, View, Animated, Easing, Text } from "react-native";
-import Logo from "../assets/svg/icons/"
+import { View, Animated, Easing, Text } from "react-native";
+import Logo from "../assets/svg/icons/logo.svg";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-
   const scale = useRef(new Animated.Value(1)).current;
 
-useEffect(() => {
+  useEffect(() => {
     const anim = Animated.sequence([
       Animated.timing(scale, {
-        toValue: 0.5,
-        duration: 700,
+        toValue: 0.7,
+        duration: 300,
         easing: Easing.out(Easing.cubic),
         useNativeDriver: true,
       }),
-      Animated.delay(200),
+      Animated.delay(800),
       Animated.timing(scale, {
         toValue: 2,
-        duration: 700,
+        duration: 400,
         easing: Easing.in(Easing.quad),
         useNativeDriver: true,
       }),
       Animated.delay(1500),
     ]);
 
-    anim.start(() => {
-
-      router.replace("/screens/onboarding/Onboarding");
-    });
+    anim.start(() => {});
 
     return () => anim.stop();
   }, []);
 
   return (
-    <SafeAreaView className="flex-1 items-center justify-center  bg-yellowOrange">
+    <>
       <StatusBar hidden />
-      <View className="flex-1 justify-center items-center">
+      <LinearGradient
+        className="flex-1"
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0.8, y: 1 }}
+        colors={["#000000", "#052F5F"]}
+      >
         <Animated.View
           style={{ transform: [{ scale }] }}
           className="items-center justify-center"
         >
           <View className="flex justify-center items-center w-[812px] h-[804px] rounded-full bg-black800">
-            <View className="w-[219px] h-[219px] rounded-full absolute justify-center items-center">
-              <Logo className="w-[61px] h-[34px]" />
-              <Text className="text-[20px] text-offWhite font-nourd_bold">
-                Food<Text className="text-yellow500">Bridge</Text>
-              </Text>
-            </View>
+            <Logo width={70} height={51}/>
           </View>
         </Animated.View>
-      </View>
-    </SafeAreaView>
-  );
+      </LinearGradient>
+    </>
   );
 }
-
-
