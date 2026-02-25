9f0e979a461a7bb6cd3b6d12bbb16d897af8eec7
ooliveiratg <menezesth16@gmail.com>
2026-02-25 17:02:40 -0300

    chore: I fixed the code so that it works on iOS too

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import { View, Animated, Easing } from "react-native";
 import Logo from "../assets/svg/icons/logo.svg";
 import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   const router = useRouter();
   const scale = useRef(new Animated.Value(1)).current;
@@ -38,11 +39,12 @@ export default function App() {
     <>
       <StatusBar hidden />
       <LinearGradient
-        className="flex-1"
+        style={{ flex: 1 }}
         start={{ x: 0, y: 0 }}
         end={{ x: 0.8, y: 1 }}
         colors={["#000000", "#052F5F"]}
       >
+        <SafeAreaView style={{ flex: 1 }}>
         <Animated.View
           style={{ transform: [{ scale }] }}
           className="items-center justify-center"
@@ -51,6 +53,7 @@ export default function App() {
             <Logo width={70} height={51} />
           </View>
         </Animated.View>
+        </SafeAreaView>
       </LinearGradient>
     </>
   );
