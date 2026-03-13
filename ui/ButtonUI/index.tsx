7b3b02a4125e299bc92400a6657dd6a4f4135cf5
ooliveiratg <menezesth16@gmail.com>
2026-03-12 23:13:03 -0300

    feat(ButtonUI): Adding ternary operators for the new button layout, refactoring the code.

--- ui/ButtonUI/index.tsx
+++ ui/ButtonUI/index.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { Pressable, TouchableOpacity, View } from "react-native";
+import { Pressable, TouchableOpacity, View, Text } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { IButtonProps } from "../../interfaces/ui/ButtonUI";
 
@@ -55,12 +55,48 @@ export default function ButtonUI({
       className={` ${props.paddingButtonStatus} rounded-[20px] bg-[rgba(255,255,255,0.03)] border border-solid border-[rgba(255,255,255,0.1)] ${props.size} `}
     >
       <View className="w-full flex-row justify-between items-center">
-        <MaterialIcons />
+        <View
+          className={`py-[4px] px-[10px] gap-[8px] items-center ${props.status === "Em Análise" ? "bg-[rgba(249,115,22,0.1)] border border-solid border-[rgba(249,115,22,0.2)]" : props.status === "Concluído" ? "bg-[rgba(59,130,246,0.1)] border border-solid border-[rgba(59,130,246,0.2)]" : "bg-[rgba(34,197,94,0.1)] border border-solid border-[rgba(34,197,94,0.2)]"}`}
+        >
+          <MaterialIcons name="verified-user" size={24}/>
+        </View>
         <View className="flex-col gap-[2px]">
-          <Text>
+          <Text></Text>
+          {props.statusBorder ? (
+            <View
+              style={{
+                boxShadow: `0px 0px 10px ${props.status === "Concluído" ? "rgba(34,197,94,0.3)" : props.status === "Aguardando Advogado" ? "rgba(37,99,235,0.3)" : "rgba(249,115,22,0.3)"}`,
+              }}
+              className={`py-[4px] px-[10px] gap-[8px] items-center ${props.status === "Em Análise" ? "bg-[rgba(249,115,22,0.1)] border border-solid border-[rgba(249,115,22,0.2)]" : props.status === "Concluído" ? "bg-[rgba(59,130,246,0.1)] border border-solid border-[rgba(59,130,246,0.2)]" : "bg-[rgba(34,197,94,0.1)] border border-solid border-[rgba(34,197,94,0.2)]"}`}
+            >
+              <View
+                className={`min-w-[6px] min-h-[6px] ${props.status === "Concluído" ? "bg-[#22C55E]" : props.status === "Aguardando Advogado" ? "bg-[#3B82F6]" : "bg-[#F97316]"}`}
+              ></View>
 
-          </Text>
-          {props.status && props}
+              <Text
+                className={` font-interBold text-[9px] ${props.status === "Em Análise" ? "text-[#FB923C] " : props.status === "Aguardando Advogado" ? "text-[#60A5FA]" : "text-[#4ADE80]"}`}
+              >
+                {props.status}
+              </Text>
+            </View>
+          ) : props.status &&(
+            <View className="gap-[8px] items-center">
+              <View
+                className={`min-w-[6px] min-h-[6px] ${props.status === "Concluído" ? "bg-[#22C55E]" : props.status === "Aguardando Advogado" ? "bg-[#3B82F6]" : "bg-[#F97316]"}`}
+              ></View>
+
+              <Text
+                className={` font-interBold text-[9px] ${props.status === "Em Análise" ? "text-[#FB923C] " : props.status === "Aguardando Advogado" ? "text-[#60A5FA]" : "text-[#4ADE80]"}`}
+              >
+                {props.status}
+              </Text>
+            </View>
+          )}
+          <MaterialIcons
+            style={{ transform: [{ rotate: "-180deg" }] }}
+            name="arrow-forward"
+            size={14}
+          />
         </View>
       </View>
     </Pressable>
