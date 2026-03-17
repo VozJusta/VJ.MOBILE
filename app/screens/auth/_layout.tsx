68b701410b66d099e4cb772d2f09158e5fa47ee7
ooliveiratg <menezesth16@gmail.com>
2026-03-17 19:27:32 -0300

    feat:merge

--- app/screens/auth/_layout.tsx
+++ app/screens/auth/_layout.tsx
@@ -1,17 +1,16 @@
-import { Slot } from "expo-router";
-import Navbar from "../../../components/Navbar";
 import { LinearGradient } from "expo-linear-gradient";
+import { Slot } from "expo-router";
 
-export default function UsersLayout() {
+export default function AuthLayout() {
   return (
-   <LinearGradient
+    <LinearGradient
       style={{ flex: 1 }}
       start={{ x: 0, y: 0 }}
       end={{ x: 0.8, y: 1 }}
       colors={["#000000", "#052F5F"]}
+    
     >
       <Slot />
-      <Navbar />
     </LinearGradient>
   );
 }
