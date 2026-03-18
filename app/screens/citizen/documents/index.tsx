d639e27e19c757ce4b0cd0d66b6daadbd2d87e55
ooliveiratg <menezesth16@gmail.com>
2026-03-18 20:20:50 -0300

    feat(documents): added lib for files upload

--- app/screens/citizen/documents/index.tsx
+++ app/screens/citizen/documents/index.tsx
@@ -1,8 +1,21 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Logo from "@/assets/svg/icons/logo.svg";
+import * as DocumentPicker from 'expo-document-picker';
 
 export default function DocumentsCitizen() {
+    const pickFile = async () => {
+    let result = await DocumentPicker.getDocumentAsync({
+      type: '*/*', // aceita qualquer tipo de arquivo
+      copyToCacheDirectory: true,
+    });
+
+    console.log(result);
+
+    if (result.canceled === false) {
+      console.log('Arquivo selecionado:', result.assets[0]);
+    }
+  };
     return (
         <SafeAreaView style={{ flex: 1 }}
             className="pt-[32px] px-[16px] gap-[32px]"
@@ -12,6 +25,9 @@ export default function DocumentsCitizen() {
                     Documentos
                 </Text>
                 <Logo width={40} height={29} />
+            </View>
+            <View className="w-full py-[32px] bg-[rgba(255,255,255,0.03)] border border-solid border-[rgba(19,91,236,0.3)] rounded-[48px]">
+            
             </View>
         </SafeAreaView>
     )
