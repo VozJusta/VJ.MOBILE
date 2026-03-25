3b2aa6d9d5ffad9aee1a2fe14398ed0a6f385c5e
ooliveiratg <menezesth16@gmail.com>
2026-03-25 00:11:57 -0300

    feat(listCases): finalizando a tela de listagem de casos

---
+++ src/utils/home/cases/data.ts
@@ -0,0 +1,46 @@
+import { IButtonCases } from "@/interfaces/utils/cases/interface";
+
+export const casesData: IButtonCases[] = [
+    {
+        title: "Ação Trabalhista - XPTO",
+        icon: "work",
+        color: "orange",
+        status: "Em Análise",
+        path: ""
+    },
+    {
+        title: "Indenização Danos Morais",
+        icon: "verified",
+        color: "green",
+        status: "Concluído",
+        path: ""
+    },
+    {
+        title: "Divórcio Consensual",
+        icon: "shield",
+        color: "blue",
+        status: "Aguardando Advogado",
+        path: ""
+    },
+     {
+        title: "Revisão de Contrato",
+        icon: "article",
+        color: "orange",
+        status: "Em Análise",
+        path: ""
+    },
+    {
+        title: "Pensão Alimentícia",
+        icon: "gavel",
+        color: "green",
+        status: "Concluído",
+        path: ""
+    },
+    {
+        title: "Distrato Imobiliário",
+        icon: "description",
+        color: "blue",
+        status: "Aguardando Advogado",
+        path: ""
+    },
+]
