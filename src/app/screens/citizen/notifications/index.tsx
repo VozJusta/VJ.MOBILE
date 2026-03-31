cc8dbe5d527ec617fe9a184949c964f521b339b1
xsalles <xs.salles@gmail.com>
2026-03-30 21:24:53 -0300

    feat(component): added the empty state of flatlist

--- src/app/screens/citizen/notifications/index.tsx
+++ src/app/screens/citizen/notifications/index.tsx
@@ -1,22 +1,10 @@
-import {
-  TypeNotificationBgColor,
-  TypeNotificationColor,
-  TypeNotificationIcon,
-} from "@/interfaces/components/TypeNotificationCard";
+
 import { NotificationTemplateProps } from "@/interfaces/template/NotificationTemplate";
 import NotificationTemplate from "@/template/notification";
 
 export const data: NotificationTemplateProps = {
   notifications: [
-    {
-      id: "1",
-      title: "Nova denúncia recebida",
-      description: "Você recebeu uma nova denúncia sobre poluição sonora.",
-      date: new Date("2024-06-01T10:30:00"),
-      icon: TypeNotificationIcon.NOTIFICATION,
-      iconColor: TypeNotificationColor.NOTIFICATION,
-      bgColor: TypeNotificationBgColor.NOTIFICATION,
-    },
+    
   ],
 };
 
