d97a565cbeaffd31b53ebd0d5fa73b1aedbab3f1
xsalles <xs.salles@gmail.com>
2026-03-30 21:24:53 -0300

    feat(component): added the empty state of flatlist

--- src/template/notification/index.tsx
+++ src/template/notification/index.tsx
@@ -3,8 +3,9 @@ import NotificationCard from "@/components/NotificationCard";
 import { NotificationTemplateProps } from "@/interfaces/template/NotificationTemplate";
 import ButtonUI from "@/ui/ButtonUI";
 import { MaterialIcons } from "@expo/vector-icons";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyNotification from "@/assets/svg/illustration-empty-notification.svg";
 
 export default function NotificationTemplate({
   notifications,
@@ -13,10 +14,23 @@ export default function NotificationTemplate({
     <SafeAreaView className="flex-1 px-4 py-6 gap-4">
       <Header isFirstPage={false} title="NOTIFICAÇÕES"></Header>
       <FlatList
+        className="mt-8"
         data={notifications}
         renderItem={({ item: notification }) => (
           <NotificationCard {...notification} />
         )}
+        ListEmptyComponent={
+          <View className="flex flex-col items-center gap-2">
+            <EmptyNotification width={200} height={200} />
+            <Text className="font-interSemiBold text-[32px] text-white">
+              Tudo limpo por aqui!
+            </Text>
+            <Text className="font-interRegular text-[14px] text-[#94A3B8] w-fit h-fit text-center">
+              Você não tem nenhuma notificação nova no momento. Avisaremos assim
+              que algo importante acontecer.
+            </Text>
+          </View>
+        }
         keyExtractor={(item) => item.id}
       />
       <ButtonUI
