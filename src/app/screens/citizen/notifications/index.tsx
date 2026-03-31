186afb84a82502e58f36846acce66172548402fc
xsalles <xs.salles@gmail.com>
2026-03-30 21:10:31 -0300

    refactored(architecture): adjusted the structure of interfaces and types in the folders

--- src/app/screens/citizen/notifications/index.tsx
+++ src/app/screens/citizen/notifications/index.tsx
@@ -1,21 +1,25 @@
-import { NotificationCardProps } from "@/components/NotificationCard";
-import { TypeNotificationBgColor, TypeNotificationColor, TypeNotificationIcon } from "@/components/TypeNotificationCard";
+import {
+  TypeNotificationBgColor,
+  TypeNotificationColor,
+  TypeNotificationIcon,
+} from "@/interfaces/components/TypeNotificationCard";
+import { NotificationTemplateProps } from "@/interfaces/template/NotificationTemplate";
 import NotificationTemplate from "@/template/notification";
 
-export const data: NotificationCardProps[] = [
+export const data: NotificationTemplateProps = {
+  notifications: [
     {
-        id: "1",
-        title: "Nova denúncia recebida",
-        description: "Você recebeu uma nova denúncia sobre poluição sonora.",
-        date: new Date("2024-06-01T10:30:00"),
-        icon: TypeNotificationIcon.NOTIFICATION,
-        iconColor: TypeNotificationColor.NOTIFICATION,
-        bgColor: TypeNotificationBgColor.NOTIFICATION,
-    }
-]
+      id: "1",
+      title: "Nova denúncia recebida",
+      description: "Você recebeu uma nova denúncia sobre poluição sonora.",
+      date: new Date("2024-06-01T10:30:00"),
+      icon: TypeNotificationIcon.NOTIFICATION,
+      iconColor: TypeNotificationColor.NOTIFICATION,
+      bgColor: TypeNotificationBgColor.NOTIFICATION,
+    },
+  ],
+};
 
 export default function Notifications() {
-    return (
-        <NotificationTemplate notifications={data} />
-    )
+  return <NotificationTemplate notifications={data.notifications} />;
 }
--- src/components/TypeNotificationCard/index.tsx
+++ src/components/TypeNotificationCard/index.tsx
@@ -1,32 +1,6 @@
+import { TypeNotificationCardProps } from "@/interfaces/components/TypeNotificationCard";
 import { MaterialIcons } from "@expo/vector-icons";
 import { View } from "react-native";
-
-export enum TypeNotificationIcon {
-  NOTIFICATION = "notifications",
-  ALERT = "warning",
-  DOCUMENTS = "article",
-  SUCCESS = "verified",
-}
-
-export enum TypeNotificationColor {
-  NOTIFICATION = "#2B66E3",
-  ALERT = "#FDD835",
-  DOCUMENTS = "#F97316",
-  SUCCESS = "#10B981",
-}
-
-export enum TypeNotificationBgColor {
-  NOTIFICATION = "#0B192F",
-  ALERT = "#FFEDD5",
-  DOCUMENTS = "#575343",
-  SUCCESS = "#161315",
-}
-
-export interface TypeNotificationCardProps {
-  icon: TypeNotificationIcon;
-  iconColor?: string;
-  bgColor?: string;
-}
 
 export default function TypeNotificationCard({...props}: TypeNotificationCardProps) {
   return (
