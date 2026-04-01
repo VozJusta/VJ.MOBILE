e2b1624ff2ab6549b9ed980f6c3220156ca22e63
xsalles <xs.salles@gmail.com>
2026-04-01 13:51:08 -0300

    feat(lawyer): created the section of relevants request and adjusted some issues of UX

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -5,13 +5,13 @@ export default function Badge({ ...props }: BadgeProps) {
   return (
     <View
       className="flex text-center items-center py-0.5 px-2 rounded"
-      style={{ backgroundColor: props.bgColor }}
+      style={{ backgroundColor: props.badgeColor + "33" }} // Esse "33" é para adicionar 20% de opacidade
     >
       <Text
         className="font-interBold text-[10px]"
-        style={{ color: props.textColor }}
+        style={{ color: props.badgeColor }}
       >
-        {props.text}
+        {props.textBadge}
       </Text>
     </View>
   );
--- src/components/RequestCard/index.tsx
+++ src/components/RequestCard/index.tsx
@@ -5,7 +5,7 @@ import { RequestCardProps } from "@/interfaces/components/RequestCard";
 
 export default function RequestCard(props: RequestCardProps) {
   return (
-    <View className="flex flex-col w-full p-5 gap-6 bg-[#161E29]/70 border border-[#2B86EE]/20 rounded-2xl">
+    <View className="flex flex-col w-full gap-6 p-5 bg-[#161E29]/70 border border-[#2B86EE]/20 rounded-2xl">
       <View className="flex flex-col items-start gap-3">
         <Text className="font-interBold text-[16px] text-white">
           {props.title}
@@ -13,14 +13,14 @@ export default function RequestCard(props: RequestCardProps) {
         <Badge {...props} />
       </View>
       <View className="flex flex-row items-center justify-between ">
-        <View className="flex flex-col pr-6">
+        <View className="flex flex-col pr-6 border-r border-[#2B86EE]/10">
           <Text className="font-inter text-[14px] text-[#94A3B8]">SCORE</Text>
           <Text className="font-interBold text-[20px] text-[#34D399]">
             {props.score}
           </Text>
         </View>
         <View className="flex flex-row items-center gap-4">
-          <View className="flex flex-col gap-2.5">
+          <View className="flex flex-col gap-1">
             <Text className="font-inter text-[14px] text-[#94A3B8]">
               Relatório do caso
             </Text>
@@ -35,7 +35,7 @@ export default function RequestCard(props: RequestCardProps) {
                 "Esta funcionalidade está em desenvolvimento ainda.",
               )
             }
-            className="flex rounded-full py-3 px-2 items-center justify-center bg-[#161E29] border-[#2B86EE]/20"
+            className="flex rounded-full py-3 px-2 items-center justify-center bg-[#161E29] border-[#2B86EE]/20 border"
           >
             <MaterialIcons name="visibility" size={20} color={"#2B86EE"} />
           </TouchableOpacity>
