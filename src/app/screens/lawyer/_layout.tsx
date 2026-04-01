2365e91f89cbc479ba3299f4114c005a142ae440
xsalles <xs.salles@gmail.com>
2026-04-01 17:14:08 -0300

    fix(style): adjusted the padding bottom

--- src/app/screens/lawyer/_layout.tsx
+++ src/app/screens/lawyer/_layout.tsx
@@ -5,7 +5,7 @@ import { LinearGradient } from "expo-linear-gradient";
 export default function LawyerLayout() {
   return (
     <LinearGradient
-      style={{ flex: 1, paddingTop: 32, paddingInline: 16 }}
+      style={{ flex: 1, paddingBottom: 84, paddingTop: 32, paddingInline: 16 }}
       start={{ x: 0, y: 0 }}
       end={{ x: 0.8, y: 1 }}
       colors={["#000000", "#052F5F"]}
