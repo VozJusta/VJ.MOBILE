60629b04f969b47d38ee7ab75eac9604c2adaa26
ooliveiratg <menezesth16@gmail.com>
2026-04-05 21:58:26 -0300

    feat(token): retrieving the token and adding it to asyncStorage using zustand

--- src/services/users/citizen/validateEmail.ts
+++ src/services/users/citizen/validateEmail.ts
@@ -1,11 +1,21 @@
+import { ITokenService } from "@/interfaces/services/token/token";
 import { BASE_URL } from "@/services/BASE_URL";
+import {
+  useAccessTokenStorage,
+  useRefreshTokenStorage,
+} from "@/store/token.store";
 import { Alert } from "react-native";
+import { set } from "zod";
 
 export async function ValidateEmail(
   email: string,
   code: string,
   token: string,
 ) {
+  console.log("🔥 ENTROU NO ValidateEmail");
+  const setAccessToken = useAccessTokenStorage.getState().setTokens;
+  const setRefreshToken = useRefreshTokenStorage.getState().setTokens;
+
   try {
     const response = await fetch(`${BASE_URL}/auth/validate/email`, {
       method: "POST",
@@ -19,6 +29,9 @@ export async function ValidateEmail(
         code: code,
       }),
     });
+    const jsonResponse: ITokenService = await response.json();
+    setAccessToken(jsonResponse.access_token || "");
+    setRefreshToken(jsonResponse.refresh_token || "");
 
     if (!response.ok) {
       return {
@@ -27,13 +40,9 @@ export async function ValidateEmail(
       };
     }
 
-    const json = await response.json();
-    console.log("JSON da validação de email:", json.access_token);
-    console.log("Resposta da validação de email (raw):", response);
-
     return {
       success: true,
-      data: json,
+      data: "",
     };
   } catch (err: any) {
     console.log("ERRO NA REQUISIÇÃO:", err);
