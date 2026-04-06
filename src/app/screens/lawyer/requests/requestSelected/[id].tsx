ca07e7fa0511272f5afca247fde7065e82b2d63a
xsalles <xs.salles@gmail.com>
2026-04-06 13:07:55 -0300

    feat(caseSelected): created the component of analysys card and put in the page that user view when click in one request

--- src/app/screens/lawyer/requests/requestSelected/[id].tsx
+++ src/app/screens/lawyer/requests/requestSelected/[id].tsx
@@ -1,35 +1,49 @@
 import Header from "@/components/Header";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Alert, Text, TouchableOpacity } from "react-native";
+import { Alert, ScrollView, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { requestsCards } from "../data";
 import RequestCard from "@/components/RequestCard";
+import { requestSelectedAnalysysData } from "./data";
+import AnalysysCard from "@/components/AnalysysCard";
 
 export default function RequestSelected() {
   const { id } = useLocalSearchParams();
 
-  const requestSelected = requestsCards.filter(
-    (request) => request.id === id,
-  );
+  const requestSelected = requestsCards.filter((request) => request.id === id);
 
   return (
-    <SafeAreaView className="flex-1 gap-6">
-      <Header title={requestSelected[0].nameCase.toUpperCase()} isFirstPage={false} isCitizen={false} />
+    <ScrollView>
+      <SafeAreaView className="flex-1 gap-6">
+        <Header
+          title={requestSelected[0].nameCase.toUpperCase()}
+          isFirstPage={false}
+          isCitizen={false}
+        />
 
-      <RequestCard {...requestSelected[0]} />
+        <RequestCard {...requestSelected[0]} />
 
-      <TouchableOpacity
-        onPress={() =>
-          Alert.alert(
-            "Parâmetros da solicitação",
-            JSON.stringify(requestSelected[0]),
-          )
-        }
-        className="mt-4 px-4 py-2 bg-[#2B86EE] rounded-lg"
-      >
-        <Text className="text-white font-interSemiBold">Ver parametros</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+        <Text className="font-interSemiBold text-[20px] text-white">
+          Análise Jurídica da IA
+        </Text>
+
+        {requestSelectedAnalysysData.map((analysys, index) => (
+          <AnalysysCard key={index} {...analysys} />
+        ))}
+
+        <TouchableOpacity
+          onPress={() =>
+            Alert.alert(
+              "Parâmetros da solicitação",
+              JSON.stringify(requestSelected[0]),
+            )
+          }
+          className="mt-4 px-4 py-2 bg-[#2B86EE] rounded-lg"
+        >
+          <Text className="text-white font-interSemiBold">Ver parametros</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
