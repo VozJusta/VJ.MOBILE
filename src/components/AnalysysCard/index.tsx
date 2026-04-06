6179d79e7d02627d2dca85b756d7f81d5a696381
xsalles <xs.salles@gmail.com>
2026-04-06 13:07:55 -0300

    feat(caseSelected): created the component of analysys card and put in the page that user view when click in one request

---
+++ src/components/AnalysysCard/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { AnalysysCardProps } from "@/interfaces/components/AnalysysCard";
+
+export default function AnalysysCard({ ...props }: AnalysysCardProps) {
+  return (
+    <View className="flex flex-col w-full h-fit border-l-4 rounded-xl p-5 bg-[#161E29]/70 gap-6 border-l-[#2B86EE]">
+      <View className="flex flex-col gap-4 text-start">
+        <Text className="font-interSemiBold text-[16px] text-white">
+          {props.title}
+        </Text>
+        <Text className="font-interRegular text-[12px] text-[#FFFFFF]">
+          {props.text}
+        </Text>
+      </View>
+    </View>
+  );
+}
