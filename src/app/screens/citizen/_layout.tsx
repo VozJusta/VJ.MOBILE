e2db12d709d0f4150132566aa9811c4691ca83eb
xsalles <xs.salles@gmail.com>
2026-04-15 01:14:04 -0300

    refactored(style): now, depending of the route that citizen is, the padding is not showed as is it's in those routes of navbar

--- src/app/screens/citizen/_layout.tsx
+++ src/app/screens/citizen/_layout.tsx
@@ -12,7 +12,7 @@ export default function CitizenLayout() {
 
   return (
     <LinearGradient
-      style={{ flex: 1, paddingBottom: 128, paddingTop: 32, paddingInline: 16 }}
+      style={{ flex: 1, paddingBottom: navbarRoutes.includes(pathName) ? 128 : 32, paddingTop: 32, paddingInline: 16 }}
       start={{ x: 0, y: 0 }}
       end={{ x: 0.8, y: 1 }}
       colors={["#000000", "#052F5F"]}
