cddb69344a7faa72f3ab1809a3ed09e55a35de86
xsalles <xs.salles@gmail.com>
2026-04-20 17:10:50 -0300

    feat(screen): connected the informations with the api

--- src/app/screens/citizen/home/listCases/caseSelected/[id].tsx
+++ src/app/screens/citizen/home/listCases/caseSelected/[id].tsx
@@ -14,16 +14,18 @@ import { getCategoryLabel, translateStatus } from "@/utils/screens/citizen/home"
 
 export default function CaseSelected() {
   const router = useRouter();
-  const local = useLocalSearchParams();
+  const local = useLocalSearchParams<{ id?: string | string[] }>();
   const [reportData, setReportData] = useState<IGetReportDetailsResponse | undefined>(undefined);
   const { getDetailsReportById, loading } = useDashboard();
 
+  console.log("ID do relatório selecionado:", local.id);
+
   useEffect(() => {
     async function load() {
-      if (!local.id) return;
+      const reportId = Array.isArray(local.id) ? local.id[0] : local.id;
+      if (!reportId) return;
 
-
-      const data = await getDetailsReportById(local.id[0]);
+      const data = await getDetailsReportById(reportId);
 
       if (data) {
         setReportData(data);
@@ -50,13 +52,13 @@ export default function CaseSelected() {
             {reportData && getCategoryLabel(reportData?.user.report.category_detected)}
           </Text>
         </View>
-        <View className="py-[24px] pl-[24px] pr-[37px] bg-[rgba(15,23,42,0.7)] border border-solid border-[rgba(255,255,255,0.1)] rounded-[24px]">
+        <View className="py-[24px] pl-[24px] gap-2 pr-[37px] bg-[rgba(15,23,42,0.7)] border border-solid border-[rgba(255,255,255,0.1)] rounded-[24px]">
           <Text className="text-[14px] font-interSemiBold text-[#2563EB]">
-            Status Atual
+            STATUS ATUAL
           </Text>
           <View className="flex-row gap-[8px] items-center ">
             <Text className="text-[24px] font-interBold text-white">
-              {reportData && translateStatus(reportData?.user.report.status)}
+              {reportData && translateStatus(reportData?.user.report.status).toUpperCase()}
             </Text>
             <View className="w-[12px] h-[12px] rounded-full bg-[#2563EB]"></View>
           </View>
