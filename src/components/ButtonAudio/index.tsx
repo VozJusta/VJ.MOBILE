762433b256029d11562e61b74ab1324329d455be
xsalles <xs.salles@gmail.com>
2026-04-19 23:29:43 -0300

    refactored(component): changed the bg color depending of the state

--- src/components/ButtonAudio/index.tsx
+++ src/components/ButtonAudio/index.tsx
@@ -4,26 +4,24 @@ import { View, TouchableOpacity } from "react-native";
 
 export default function ButtonAudio(props: IButtonAudio) {
   return (
-    <View className="flex items-center justify-center p-2">
-      <TouchableOpacity
-        onPress={
-          props.isRecording ? props.onStopRecording : props.onStartRecording
-        }
-        disabled={props.disabled}
-        className={`flex h-12 w-12 items-center justify-center rounded-full shadow-sm ${
-          props.disabled
-            ? "bg-slate-400"
-            : props.isRecording
-              ? "bg-red-500"
-              : "bg-blue-600"
-        }`}
-      >
-        <MaterialIcons
-          name={props.isRecording ? "stop" : "mic"}
-          size={24}
-          color="white"
-        />
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      onPress={
+        props.isRecording ? props.onStopRecording : props.onStartRecording
+      }
+      disabled={props.disabled}
+      className={`flex h-12 w-12 items-center justify-center rounded-full shadow-sm ${
+        props.disabled
+          ? "bg-slate-400"
+          : props.isRecording
+            ? "bg-[#EF4444]"
+            : "bg-[#2563EB]"
+      }`}
+    >
+      <MaterialIcons
+        name={props.isRecording ? "stop" : "mic"}
+        size={24}
+        color="white"
+      />
+    </TouchableOpacity>
   );
 }
