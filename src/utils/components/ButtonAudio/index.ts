864edfa17c2d0a6c0f76d1ba6ec48e6fe80dc134
xsalles <xs.salles@gmail.com>
2026-04-19 23:41:38 -0300

    feat(utils): created a function to format the time of record

--- src/utils/components/ButtonAudio/index.ts
+++ src/utils/components/ButtonAudio/index.ts
@@ -1,6 +1,23 @@
-const getBarHeight = (baseHeight: number, modifier: number = 1, meteringVoice: number) => {
-    const normalizedVolume = Math.max(0, Math.min(100, (meteringVoice + 160) * (100 / 160))); 
-    
-    const dynamicHeight = Math.max(4, (normalizedVolume / 100) * baseHeight * modifier);
-    return dynamicHeight;
+export const getBarHeight = (
+  baseHeight: number,
+  modifier: number = 1,
+  meteringVoice: number,
+) => {
+  const normalizedVolume = Math.max(
+    0,
+    Math.min(100, (meteringVoice + 160) * (100 / 160)),
+  );
+
+  const dynamicHeight = Math.max(
+    4,
+    (normalizedVolume / 100) * baseHeight * modifier,
+  );
+  return dynamicHeight;
+};
+
+export const formatTime = (millis: number) => {
+  const totalSeconds = Math.floor(millis / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 };
