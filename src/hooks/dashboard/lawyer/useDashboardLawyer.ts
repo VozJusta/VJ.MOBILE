8a084c5dd080473ba5f46c3783b7bfe6e8ce065a
xsalles <xs.salles@gmail.com>
2026-04-20 23:06:21 -0300

    feat(hook): created a hook that bring the data of operational stats from the service

--- src/hooks/dashboard/lawyer/useDashboardLawyer.ts
+++ src/hooks/dashboard/lawyer/useDashboardLawyer.ts
@@ -1,11 +1,15 @@
 import { IGetAnalyticsResponse } from "@/interfaces/services/dashboard/lawyer/analytics";
+import { IGetOperationalStatsResponse } from "@/interfaces/services/dashboard/lawyer/operationalStas";
 import { getAnalyticsDashboardLawyer } from "@/services/dashboard/lawyer/analytics";
+import { getOperationalStatsDashboard } from "@/services/dashboard/lawyer/operationalStats";
 import { useEffect, useState } from "react";
 import Toast from "react-native-toast-message";
 
 export const useDashboardLawyer = () => {
   const [analyticsData, setAnalyticsData] =
     useState<IGetAnalyticsResponse | null>(null);
+  const [operationalStats, setOperationalStats] =
+    useState<IGetOperationalStatsResponse | null>(null);
   const [loading, setLoading] = useState(false);
 
   const getAnalytics = async () => {
@@ -34,13 +38,46 @@ export const useDashboardLawyer = () => {
     }
   };
 
+  const getOperationalStats = async () => {
+    setLoading(true);
+
+    try {
+      const response = await getOperationalStatsDashboard();
+
+      if (response.success && response.data) {
+        setOperationalStats(response.data);
+      } else {
+        Toast.show({
+          type: "error",
+          text1: "Erro ao buscar estatísticas operacionais",
+          text2: response.fields?.[0],
+        });
+      }
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "Erro ao buscar estatísticas operacionais",
+        text2: "Tente novamente mais tarde",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    getAnalytics();
+    const fetchAllData = async () => {
+      setLoading(true);
+      await Promise.all([getAnalytics(), getOperationalStats()]);
+      setLoading(false);
+    };
+    fetchAllData();
   }, []);
 
   return {
     analyticsData,
     loading,
     getAnalytics,
+    operationalStats,
+    getOperationalStats,
   };
 };
