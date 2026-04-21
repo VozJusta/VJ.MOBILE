91cee3adbe114859f17f535169529655f1303482
xsalles <xs.salles@gmail.com>
2026-04-21 12:18:08 -0300

    feat(style): now the form is centralized and the header in fixed on top

--- src/app/screens/shared/terminate-account/index.tsx
+++ src/app/screens/shared/terminate-account/index.tsx
@@ -27,57 +27,59 @@ export default function TerminateAccount() {
   };
 
   return (
-    <SafeAreaView className="flex-1 items-center justify-center">
-      <Header title="Encerrar conta" isFirstPage={false} isCitizen={false} />
+    <SafeAreaView className="flex-1">
+      <Header title="ENCERRAR CONTA" isFirstPage={false} isCitizen={false} />
 
-      <View className="self-center w-full mt-8 px-4 bg-[#1E293B]/40 border border-[rgba(255,255,255,0.12)] rounded-[24px] gap-[24px] py-12">
-        <View className="flex-col gap-2">
-          <Text className="text-white text-[24px] font-interBold">
-            Encerrar conta
-          </Text>
-          <Text className="text-[#94A3B8] text-[14px] font-interRegular leading-7">
-            Digite sua senha para continuar com a exclusão da conta. Essa ação é
-            permanente e não pode ser desfeita. Todos os seus dados serão
-            removidos.
-          </Text>
-        </View>
-
-        <InputUI
-          label="SENHA"
-          placeholder={"••••••••"}
-          leftIcon
-          keyboardType="visible-password"
-          iconSize={24}
-          iconNameProps={"lock"}
-          type={"password"}
-          secureTextEntry={!showPassword}
-          rightIcon
-          rightIconName={showPassword ? "visibility-off" : "visibility"}
-          onRightIconPress={() => setShowPassword(!showPassword)}
-          value={password}
-          onChangeText={(e) => setPassword(e)}
-        />
-
-        <ButtonUI
-          onPress={handleTerminateAccount}
-          gradient={false}
-          hover={false}
-          bg="bg-[#EA2027]"
-          border="border border-[#F04444]"
-          size={"h-[56px] w-full rounded-[16px]"}
-          iconLeft={false}
-          paddingButtonStatus={"px-[20px]"}
-        >
-          <View className="w-full h-full items-center justify-center">
-            {loading ? (
-              <ActivityIndicator size="small" color="#FFF" />
-            ) : (
-              <Text className={"text-[14px] font-interBold text-white"}>
-                Excluir conta
-              </Text>
-            )}
+      <View className="flex-1 items-center justify-center">
+        <View className="w-full bg-[#1E293B]/40 border border-[rgba(255,255,255,0.12)] rounded-[24px] gap-[24px] py-12 px-4">
+          <View className="flex-col gap-2">
+            <Text className="text-white text-[24px] font-interBold">
+              Encerrar conta
+            </Text>
+            <Text className="text-[#94A3B8] text-[14px] font-interRegular leading-7">
+              Digite sua senha para continuar com a exclusão da conta. Essa ação é
+              permanente e não pode ser desfeita. Todos os seus dados serão
+              removidos.
+            </Text>
           </View>
-        </ButtonUI>
+
+          <InputUI
+            label="SENHA"
+            placeholder={"••••••••"}
+            leftIcon
+            keyboardType="visible-password"
+            iconSize={24}
+            iconNameProps={"lock"}
+            type={"password"}
+            secureTextEntry={!showPassword}
+            rightIcon
+            rightIconName={showPassword ? "visibility-off" : "visibility"}
+            onRightIconPress={() => setShowPassword(!showPassword)}
+            value={password}
+            onChangeText={(e) => setPassword(e)}
+          />
+
+          <ButtonUI
+            onPress={handleTerminateAccount}
+            gradient={false}
+            hover={false}
+            bg="bg-[#EA2027]"
+            border="border border-[#F04444]"
+            size={"h-[56px] w-full rounded-[16px]"}
+            iconLeft={false}
+            paddingButtonStatus={"px-[20px]"}
+          >
+            <View className="w-full h-full items-center justify-center">
+              {loading ? (
+                <ActivityIndicator size="small" color="#FFF" />
+              ) : (
+                <Text className={"text-[14px] font-interBold text-white"}>
+                  Excluir conta
+                </Text>
+              )}
+            </View>
+          </ButtonUI>
+        </View>
       </View>
     </SafeAreaView>
   );
