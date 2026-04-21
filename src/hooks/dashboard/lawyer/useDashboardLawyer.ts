fd00395b9e6ba1365c07d95090c7b703bd60e40c
xsalles <xs.salles@gmail.com>
2026-04-20 21:23:05 -0300

    feat(hook): added a function of getting analytics for the dashboard of lawyer

---
+++ src/hooks/dashboard/lawyer/useDashboardLawyer.ts
@@ -0,0 +1,42 @@
+import { IGetAnalyticsResponse } from "@/interfaces/services/dashboard/lawyer/analytics";
+import { getAnalyticsDashboardLawyer } from "@/services/dashboard/lawyer/analytics";
+import { useState } from "react";
+import Toast from "react-native-toast-message";
+
+export const useDashboardLawyer = () => {
+  const [analyticsData, setAnalyticsData] =
+    useState<IGetAnalyticsResponse | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const getAnalytics = async () => {
+    setLoading(true);
+
+    try {
+      const response = await getAnalyticsDashboardLawyer();
+
+      if (response.success && response.data) {
+        setAnalyticsData(response.data);
+      } else {
+        Toast.show({
+          type: "error",
+          text1: "Erro ao buscar analytics",
+          text2: response.fields?.[0],
+        });
+      }
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "Erro ao buscar analytics",
+        text2: "Tente novamente mais tarde",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    analyticsData,
+    loading,
+    getAnalytics,
+  };
+};
