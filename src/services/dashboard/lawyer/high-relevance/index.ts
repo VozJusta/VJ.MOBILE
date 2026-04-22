99f00ec71be00c83d66ff164527924edacbe84b7
xsalles <xs.salles@gmail.com>
2026-04-21 22:27:49 -0300

    feat(service): created the service of get high relevance cases from the lawyer

---
+++ src/services/dashboard/lawyer/high-relevance/index.ts
@@ -0,0 +1,36 @@
+import { apiFetch } from "@/helpers/api/apiFetch";
+import { IHighRelevanceResponse } from "@/interfaces/services/dashboard/lawyer/high-relevance";
+import { BASE_URL } from "@/settings/BASE_URL";
+
+export async function getHighRelevanceCases() {
+  try {
+    const response = await apiFetch(
+      `${BASE_URL}/dashboard/lawyer/high-relevance`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      },
+    );
+
+    const json = await response.json().catch(() => {});
+
+    if (!response.ok) {
+      return {
+        success: false,
+        message: json?.message,
+      };
+    }
+
+    return {
+      success: true,
+      data: json as IHighRelevanceResponse[],
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "An error occurred while fetching high relevance cases",
+    };
+  }
+}
