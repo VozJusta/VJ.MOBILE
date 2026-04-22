af5ddbcf0c7e10f739da6e1fb50e71d6fadb4f47
xsalles <xs.salles@gmail.com>
2026-04-22 20:21:38 -0300

    refactored(image): changed the image size

--- src/app/screens/citizen/home/index.tsx
+++ src/app/screens/citizen/home/index.tsx
@@ -8,16 +8,17 @@ import Header from "@/components/Header";
 import { useAccessTokenStorage } from "@/store/auth/token.store";
 import { jwtDecode } from "jwt-decode";
 import CaseCard from "@/components/CaseCard";
-import { useDashboardCitizen } from "@/hooks/dashboard/citizen/useDashboardCitizen";
 import { getCategoryLabel, getStatusIcon } from "@/utils/screens/citizen/home";
 import { useEffect } from "react";
 import { IDecodedToken } from "@/interfaces/shared/decodedToken";
 import EmptyCases from "@/assets/svg/empty-cases.svg";
+import { useDashboardCitizen } from "@/hooks/dashboard/citizen/useDashboardCitizen";
+import { IReport } from "@/interfaces/services/dashboard/citizen/reports/cards";
 
 export default function Home() {
   const router = useRouter();
   const token = useAccessTokenStorage((state) => state.accessToken);
-  const { loading, reports } = useDashboardCitizen(3);
+  const { loading, reports } = useDashboardCitizen();
 
   let decodedToken: IDecodedToken | null = null;
   if (token) {
@@ -93,7 +94,7 @@ export default function Home() {
             <ActivityIndicator size="large" color="#2563EB" className="mt-4" />
           ) : reports.length === 0 ? (
             <View className="w-full h-fit bg-[rgb(255,255,255,0.03)] border border-solid border-[rgba(255,255,255,0.1)] rounded-[12px] px-[16px] pt-[32px] gap-[6.9px] pb-[24px] flex flex-col items-center justify-center">
-              <EmptyCases width={90} height={90} />
+              <EmptyCases width={120} height={120} />
               <Text className="text-[20px] text-white font-interSemiBold">
                 Nenhum caso por aqui
               </Text>
