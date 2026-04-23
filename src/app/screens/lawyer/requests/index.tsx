213b1c5251552e92072f147af36db1f8ae909915
xsalles <xs.salles@gmail.com>
2026-04-23 12:00:02 -0300

    feat(components): created the skeleton components for the loading of cards

--- src/app/screens/lawyer/requests/index.tsx
+++ src/app/screens/lawyer/requests/index.tsx
@@ -120,19 +120,3 @@ export default function RequestsScreens() {
   );
 }
 
-const styles = StyleSheet.create({
-  shape: {
-    justifyContent: "center",
-    height: 250,
-    width: "100%",
-    borderRadius: 25,
-    marginRight: 10,
-    backgroundColor: "white",
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 16,
-  },
-});
