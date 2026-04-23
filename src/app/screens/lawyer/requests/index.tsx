556402f55ad86338ef9f3c8d0d2a705707c5e862
xsalles <xs.salles@gmail.com>
2026-04-23 20:22:08 -0300

    refactored(imports): removed some unsued imports

--- src/app/screens/lawyer/requests/index.tsx
+++ src/app/screens/lawyer/requests/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, FlatList, StyleSheet } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "@/components/Header";
