8753b8e75adf5cdb2305380441ec3cc364249282
xsalles <xs.salles@gmail.com>
2026-04-24 01:28:00 -0300

    feat(types): created an interface for the lawyer selected

---
+++ src/interfaces/services/citizen/lawyerSelected/index.ts
@@ -0,0 +1,9 @@
+export interface ILawyerSelected {
+  id: string;
+  full_name: string;
+  specialization: string;
+  avatar_image: string;
+  oab_number: string;
+  oab_state: string;
+  bio: string;
+}
