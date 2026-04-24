6ceed404bcc9d8a2463a08b1a7b5d1600b998522
xsalles <xs.salles@gmail.com>
2026-04-24 00:21:02 -0300

    feat(service): created a service to the get of lawyer list

---
+++ src/services/citizens/lawyersList/index.ts
@@ -0,0 +1,32 @@
+import { apiFetch } from "@/helpers/api/apiFetch";
+import { ILawyersList } from "@/interfaces/services/citizen/lawyersList";
+import { BASE_URL } from "@/settings/BASE_URL";
+
+export async function getLawyersList(page: number, pageSize: number) {
+  try {
+    const response = await apiFetch(
+      `${BASE_URL}/lawyers?page=${page}&pageSize=${pageSize}`,
+      {
+        method: "GET",
+      },
+    );
+
+    const json = await response.json().catch(() => ({}));
+    if (!response.ok) {
+      return {
+        success: false,
+        fields: json.message ? [json.message] : ["Erro ao buscar advogados"],
+      };
+    }
+
+    return {
+      success: true,
+      data: json as ILawyersList,
+    };
+  } catch {
+    return {
+      success: false,
+      fields: ["Erro ao buscar advogados"],
+    };
+  }
+}
