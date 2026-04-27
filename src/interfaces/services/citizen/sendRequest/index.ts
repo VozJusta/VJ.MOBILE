cfd6635d5d775e899d34297d14e53465479a2a4a
xsalles <xs.salles@gmail.com>
2026-04-27 16:40:51 -0300

    refactored(types): created an interface to the case request interface

--- src/interfaces/services/citizen/sendRequest/index.ts
+++ src/interfaces/services/citizen/sendRequest/index.ts
@@ -2,13 +2,15 @@ import { TCaseStatus } from "@/interfaces/components/CaseCard";
 
 export interface ISendRequest {
   message: string;
-  caseRequest: {
-    id: string;
-    case_id: string;
-    citizen_id: string;
-    lawyer_id: string;
-    status: TCaseStatus;
-    created_at: string;
-    updated_at: string;
-  };
+  caseRequest: ICaseRequestResponse;
+}
+
+export interface ICaseRequestResponse {
+  id: string;
+  case_id: string;
+  citizen_id: string;
+  lawyer_id: string;
+  status: TCaseStatus;
+  created_at: string;
+  updated_at: string;
 }
