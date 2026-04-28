82d85572ae792940cfe88e4437704d84f8031431
xsalles <xs.salles@gmail.com>
2026-04-28 11:10:10 -0300

    refactored(validation): separeted the zod schemas of complete register to be more organized

--- src/validation/schema.zod.ts
+++ src/validation/schema.zod.ts
@@ -9,17 +9,4 @@ import { z } from "zod";
 
 
 
-export const ZodCompleteRegisterSharedSchema = z.object({
-  cpf: cpfSchema,
-  phone: phoneSchema,
-  password: passwordSchema,
-});
 
-export const ZodCompleteRegisterCitizenSchema = ZodCompleteRegisterSharedSchema;
-
-export const ZodCompleteRegisterLawyerSchema =
-  ZodCompleteRegisterSharedSchema.extend({
-    oabNumber: oabNumberSchema,
-    oabState: z.string().min(1, "Estado da OAB é obrigatório"),
-    specialization: z.string().min(1, "Especialização é obrigatória"),
-  });
