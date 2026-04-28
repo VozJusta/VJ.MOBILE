08b8d2f665e9b5ab11b6f412db46ecaeaceafce3
xsalles <xs.salles@gmail.com>
2026-04-27 22:02:15 -0300

    refactored(function): removed a function that wasn't being used

--- src/app/screens/lawyer/requests/index.tsx
+++ src/app/screens/lawyer/requests/index.tsx
@@ -67,20 +67,6 @@ export default function RequestsScreens() {
     }
   };
 
-  const getPageNumbers = () => {
-    const maxVisible = 5;
-    let start = Math.max(1, page - Math.floor(maxVisible / 2));
-    let end = Math.min(totalPages, start + maxVisible - 1);
-
-    if (end - start < maxVisible - 1) {
-      start = Math.max(1, end - maxVisible + 1);
-    }
-
-    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
-  };
-
-  const pageNumbers = getPageNumbers();
-
   return (
     <SafeAreaView className="flex-1">
       <FlatList
