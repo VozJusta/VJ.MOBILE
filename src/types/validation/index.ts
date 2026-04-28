6d866e20d80d3235b6e8887f1e37939ac0f47cb8
xsalles <xs.salles@gmail.com>
2026-04-28 11:06:42 -0300

    feat(types): added two validation schema as a type to the typescript infer that

--- src/types/validation/index.ts
+++ src/types/validation/index.ts
@@ -1,7 +1,20 @@
-import { ZodLoginSchema, ZodSignUpLawyerSchema, ZodSignUpSchema, ZodUpdatePasswordSchema } from "@/validation/schema.zod";
+import {
+  ZodCompleteRegisterCitizenSchema,
+  ZodCompleteRegisterLawyerSchema,
+  ZodLoginSchema,
+  ZodSignUpLawyerSchema,
+  ZodSignUpSchema,
+  ZodUpdatePasswordSchema,
+} from "@/validation/schema.zod";
 import { z } from "zod";
 
 export type ZodLoginTypes = z.infer<typeof ZodLoginSchema>;
 export type ZodSignUpTypes = z.infer<typeof ZodSignUpSchema>;
 export type ZodSignUpLawyerTypes = z.infer<typeof ZodSignUpLawyerSchema>;
 export type ZodUpdatePasswordTypes = z.infer<typeof ZodUpdatePasswordSchema>;
+export type ZodCitizenCompleteRegisterTypes = z.infer<
+  typeof ZodCompleteRegisterCitizenSchema
+>;
+export type ZodLawyerCompleteRegisterTypes = z.infer<
+  typeof ZodCompleteRegisterLawyerSchema
+>;
