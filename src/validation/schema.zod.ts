1dda0e5eb34d38756091fe43814eabf88ce5e5ee
xsalles <xs.salles@gmail.com>
2026-04-28 11:09:24 -0300

    refactored(validation): separeted the zod schemas of update password to be more organized

--- src/validation/schema.zod.ts
+++ src/validation/schema.zod.ts
@@ -7,16 +7,7 @@ import {
 import { z } from "zod";
 
 
-export const ZodUpdatePasswordSchema = z
-  .object({
-    email: z.email("Email inválido").min(1, "Email é obrigatório"),
-    password: passwordSchema,
-    confirmPassword: passwordSchema,
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "As senhas não coincidem",
-    path: ["confirmPassword"],
-  });
+
 
 export const ZodCompleteRegisterSharedSchema = z.object({
   cpf: cpfSchema,
