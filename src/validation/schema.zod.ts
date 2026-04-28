4b03ebe602508cafca5d85471fb4135c1dbb5d5c
xsalles <xs.salles@gmail.com>
2026-04-28 11:08:43 -0300

    refactored(validation): separeted the zod schemas of login to be more organized

--- src/validation/schema.zod.ts
+++ src/validation/schema.zod.ts
@@ -7,12 +7,6 @@ import {
 import { z } from "zod";
 
 
-
-export const ZodLoginSchema = z.object({
-  email: z.email("Email inválido").min(1, "Email é obrigatório"),
-  password: z.string().min(1, "Senha é obrigatória"),
-});
-
 export const ZodUpdatePasswordSchema = z
   .object({
     email: z.email("Email inválido").min(1, "Email é obrigatório"),
