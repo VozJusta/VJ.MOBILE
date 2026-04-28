1316e4e42101390dabfa743432a2acf53552d84b
xsalles <xs.salles@gmail.com>
2026-04-28 14:52:23 -0300

    feat(types): created the enum of personalities in the simulation

---
+++ src/interfaces/services/citizen/simulation/index.ts
@@ -0,0 +1,8 @@
+export enum Personality {
+  CALM = "Calm",
+  AGGRESSIVE = "Agressive",
+  IMPARTIAL = "Impartial",
+  EMPATHETIC = "Empathetic",
+  PRAGMATIC = "Pragmatic",
+  RESEARCHER = "Researcher",
+}
