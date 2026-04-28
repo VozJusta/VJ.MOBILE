071234ef18926d2e2e338910b17effc33add429d
xsalles <xs.salles@gmail.com>
2026-04-28 16:27:07 -0300

    feat(types): created an interface simulation finished

--- src/interfaces/websocket/index.ts
+++ src/interfaces/websocket/index.ts
@@ -1,10 +1,21 @@
+import { TCaseStatus } from "../components/CaseCard";
+
+export type TSimulationStatus =
+  | "Waiting"
+  | "InProgress"
+  | "Completed"
+  | "TimedOut";
 
 export interface IWebSocketSimulationStarted {
-    simulationId: string;
+  simulationId: string;
 }
 
 export interface IWebSocketSimulationWarning {
-    message: string;
-    remainingSecs: number;
+  message: string;
+  remainingSecs: number;
 }
 
+export interface IWebSocketSimulationFinished {
+  simulationId: string;
+  status: TCaseStatus;
+}
