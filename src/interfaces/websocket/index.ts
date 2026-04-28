08576f40f726dae8882f198c8b9112e9efd56a50
xsalles <xs.salles@gmail.com>
2026-04-28 16:31:20 -0300

    feat(types): created an interface to the websocket simulation

--- src/interfaces/websocket/index.ts
+++ src/interfaces/websocket/index.ts
@@ -1,4 +1,9 @@
 import { TCaseStatus } from "../components/CaseCard";
+import {
+  ISimulationResponse,
+  Personality,
+} from "../services/citizen/simulation";
+import { Socket } from "socket.io-client";
 
 export type TSimulationStatus =
   | "Waiting"
@@ -18,4 +23,17 @@ export interface IWebSocketSimulationWarning {
 export interface IWebSocketSimulationFinished {
   simulationId: string;
   status: TCaseStatus;
+}
+
+export interface IWebSocketSimulation {
+  simulation: ISimulationResponse;
+  isLoading: boolean;
+  error: string | null;
+  warning: IWebSocketSimulationWarning | null;
+  simulationStatus: TSimulationStatus;
+  socket: Socket | null;
+
+  createAndStartSimulation: (personality: Personality) => Promise<void>;
+  stopSimulation: () => void;
+  clearSimulation: () => void;
 }
