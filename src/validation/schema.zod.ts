8d17a2b4291f5efea92306d9cdc26ebc7782af55
xsalles <xs.salles@gmail.com>
2026-04-28 11:07:56 -0300

    refactored(validation): separeted the zod schemas to be more organized

--- src/validation/schema.zod.ts
+++ src/validation/schema.zod.ts
@@ -6,19 +6,7 @@ import {
 } from "@/utils/validation";
 import { z } from "zod";
 
-export const ZodSignUpSchema = z.object({
-  fullName: z.string(),
-  email: z.email("Email inválido"),
-  password: passwordSchema,
-  phone: phoneSchema,
-  cpf: cpfSchema,
-});
 
-export const ZodSignUpLawyerSchema = ZodSignUpSchema.extend({
-  oabNumber: oabNumberSchema,
-  oabState: z.string().min(1, "Estado da OAB é obrigatório"),
-  specialization: z.string().min(1, "Especialização é obrigatória"),
-});
 
 export const ZodLoginSchema = z.object({
   email: z.email("Email inválido").min(1, "Email é obrigatório"),
