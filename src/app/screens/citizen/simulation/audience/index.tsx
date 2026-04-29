3bf27080b823b6b0a5ccef116226a2f4ed9ca79f
xsalles <xs.salles@gmail.com>
2026-04-29 14:38:53 -0300

    feat(screen): created the screen of simulation audience

--- src/app/screens/citizen/simulation/audience/index.tsx
+++ src/app/screens/citizen/simulation/audience/index.tsx
@@ -4,8 +4,20 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "@/components/Header";
 import ButtonAudio from "@/components/ButtonAudio";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useSimulationAudience } from "@/hooks/simulation/useSimulationAudience";
+import ButtonUI from "@/ui/ButtonUI";
+import { router } from "expo-router";
 
 export default function SimulationAudience() {
+  const {
+    handleStartRecording,
+    handleStopRecording,
+    handleStop,
+    isRecording,
+    isLoading,
+    simulationStatus,
+  } = useSimulationAudience();
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <SafeAreaView className="gap-8">
@@ -18,12 +30,36 @@ export default function SimulationAudience() {
         <View className="bg-black w-full h-[100px] rounded-xl" />
 
         <View className="flex flex-row w-full h-fit gap-6">
-          <ButtonAudio />
+          <ButtonAudio
+            isRecording={isRecording}
+            onStartRecording={handleStartRecording}
+            onStopRecording={handleStopRecording}
+            disabled={isLoading}
+          />
 
-          <TouchableOpacity className="flex h-12 w-12 items-center justify-center rounded-full shadow-sm bg-white">
-            <MaterialIcons name="stop" size={24} color="white" />
+          <TouchableOpacity
+            onPress={handleStop}
+            className="flex h-12 w-12 items-center justify-center rounded-full shadow-sm bg-white"
+          >
+            <MaterialIcons name="stop" size={24} color="black" />
           </TouchableOpacity>
         </View>
+
+        {(simulationStatus === "Completed" ||
+          simulationStatus === "TimedOut") && (
+          <ButtonUI
+            onPress={() => router.push("/screens/citizen/simulation/report")}
+            gradient={true}
+            hover={false}
+            iconLeft={false}
+            paddingButtonStatus={""}
+            children={
+              <Text className="text-white font-interSemiBold text-[16px]">
+                Encerrar e Ver Feedback
+              </Text>
+            }
+          />
+        )}
       </SafeAreaView>
     </ScrollView>
   );
