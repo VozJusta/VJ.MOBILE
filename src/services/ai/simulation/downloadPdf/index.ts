98fd0d81fbf1a1270533abd6ca628a6423e8aeac
xsalles <xs.salles@gmail.com>
2026-04-29 16:43:11 -0300

    feat(service): created the service of download the report of simulation as pdf

---
+++ src/services/ai/simulation/downloadPdf/index.ts
@@ -0,0 +1,54 @@
+import { BASE_URL } from "@/settings/BASE_URL";
+import { useAccessTokenStorage } from "@/store/auth/token.store";
+import * as FileSystem from "expo-file-system/legacy";
+import * as Sharing from "expo-sharing";
+
+export async function downloadReportSimulationAsPdf(simulationId: string) {
+  const token = useAccessTokenStorage.getState().accessToken;
+
+  try {
+    const fileUri =
+      FileSystem.documentDirectory + `simulacao_vozjusta_${simulationId}.pdf`;
+
+    const downloadRes = await FileSystem.downloadAsync(
+      `${BASE_URL}/simulation/pdf/${simulationId}`,
+      fileUri,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/pdf",
+        },
+      },
+    );
+
+    if (downloadRes.status !== 200) {
+      return {
+        success: false,
+        fields:
+          downloadRes.status === 404
+            ? ["Relatório não encontrado"]
+            : ["Erro ao baixar PDF"],
+      };
+    }
+
+    const isSharingAvailable = await Sharing.isAvailableAsync();
+
+    if (isSharingAvailable) {
+      await Sharing.shareAsync(downloadRes.uri, {
+        mimeType: "application/pdf",
+        dialogTitle: "Compartilhar Relatório PDF",
+        UTI: "com.adobe.pdf",
+      });
+    }
+
+    return {
+      success: true,
+      data: downloadRes.uri,
+    };
+  } catch {
+    return {
+      success: false,
+      fields: ["Erro de conexão com o servidor"],
+    };
+  }
+}
