f4a985bb569eb7b912866976908e5ec8ca308807
xsalles <xs.salles@gmail.com>
2026-04-30 14:59:04 -0300

    refactored(service): added another return because it was returning undefined

--- src/services/d-id/poolTalk/index.ts
+++ src/services/d-id/poolTalk/index.ts
@@ -7,33 +7,19 @@ export async function pollTalk(talkId: string) {
   try {
     for (let i = 0; i < maxAttempts; i++) {
       await new Promise((res) => setTimeout(res, interval));
-
       const response = await fetch(`${BASE_DID_URL}/talks/${talkId}`, {
-        headers: {
-          Authorization: `Basic ${BASE_DID_KEY}`,
-        },
+        headers: { Authorization: `Basic ${BASE_DID_KEY}` },
       });
-
       const data = await response.json();
 
-      if (data.status === "done") {
-        return {
-          success: true,
-          data: data.result_url as string,
-        };
-      }
-
-      if (data.status === "error") {
-        return {
-          success: false,
-          data: "Erro ao processar vídeo",
-        };
-      }
+      if (data.status === "done")
+        return { success: true, data: data.result_url as string };
+      if (data.status === "error")
+        return { success: false, data: "Erro ao processar vídeo" };
     }
+
+    return { success: false, data: "Tempo limite excedido" };
   } catch {
-    return {
-      success: false,
-      data: "Erro ao processar vídeo",
-    };
+    return { success: false, data: "Erro ao processar vídeo" };
   }
 }
