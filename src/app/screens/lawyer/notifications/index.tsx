f429f25d95d6c519e467004faae4d2423118bc5a
xsalles <xs.salles@gmail.com>
2026-05-04 16:58:20 -0300

    feat(screen): created the screen of notifications for the lawyer using the template

--- src/app/screens/lawyer/notifications/index.tsx
+++ src/app/screens/lawyer/notifications/index.tsx
@@ -1,7 +1,36 @@
+import { useNotifications } from "@/hooks/notifications/useNotifications";
+import { deleteAllNotifications } from "@/services/shared/notifications/deleteNotifications/all";
+import { updateAllNotifications } from "@/services/shared/notifications/updateNotification/all";
 import NotificationTemplate from "@/template/notification";
 
-export default function LawyerNotifications() {
-    return (
-        <NotificationTemplate notifications={[]} />
-    )
+export default function Notifications() {
+  const {
+    recentNotifications,
+    previousNotifications,
+    loading,
+    page,
+    totalPages,
+    hasNextPage,
+    hasPreviousPage,
+    goToNextPage,
+    goToPreviousPage,
+    goToPage,
+  } = useNotifications();
+
+  return (
+    <NotificationTemplate
+      recent={recentNotifications}
+      previous={previousNotifications}
+      loading={loading}
+      page={page}
+      totalPages={totalPages}
+      hasNextPage={hasNextPage}
+      hasPreviousPage={hasPreviousPage}
+      goToNextPage={goToNextPage}
+      goToPreviousPage={goToPreviousPage}
+      goToPage={goToPage}
+      onDeleteAll={deleteAllNotifications}
+      onMarkAllAsRead={updateAllNotifications}
+    />
+  );
 }
