33166a05375ea071f43bf1cf7fab0d5b40aa7338
xsalles <xs.salles@gmail.com>
2026-05-04 15:09:56 -0300

    feat(hooks): created the function of list notifications

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { INotification } from "./services/shared/notifications/listAllNotifications";
+import { listNotifications } from "@/services/shared/notifications/listNotifications";
 
 export function useNotifications() {
-    const [notifications, setNotifications] = useState<INotification[]>([]);
-    const [unreadCount, setUnreadCount] = useState<number>(0);
-    const [loading, setLoading] = useState<boolean>(false);
+  const [notifications, setNotifications] = useState<INotification[]>([]);
+  const [unreadCount, setUnreadCount] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    listNotifications().then((res) => {
+      if (res.success && res.data) {
+        setNotifications(res.data.items);
+        setUnreadCount(res.data.items.filter((n) => !n.is_read).length);
+      }
+      setLoading(false);
+    });
+  }, []);
 }
