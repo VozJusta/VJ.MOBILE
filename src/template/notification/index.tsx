4c413fb06e22f5393945f8075b88aec72e99ac68
xsalles <xs.salles@gmail.com>
2026-05-05 09:57:32 -0300

    refactored(conditional): the button was being shoed even if the notifications were empty

--- src/template/notification/index.tsx
+++ src/template/notification/index.tsx
@@ -91,21 +91,28 @@ export default function NotificationTemplate({
             }
             stickySectionHeadersEnabled={false}
           />
-          <ButtonUI
-            iconLeft={true}
-            gradient={true}
-            hover={false}
-            paddingButtonStatus="px-[16px] py-[12px] my-4"
-            onPress={onDeleteAll}
-            children={
-              <View className="w-full h-full flex-row items-center justify-center gap-2">
-                <MaterialIcons name="delete-sweep" size={20} color="#FFFFFF" />
-                <Text className="font-interSemiBold text-[14px] text-white">
-                  Limpar tudo
-                </Text>
-              </View>
-            }
-          />
+
+          {sections.length > 0 && (
+            <ButtonUI
+              iconLeft={true}
+              gradient={true}
+              hover={false}
+              paddingButtonStatus="px-[16px] py-[12px] my-4"
+              onPress={onDeleteAll}
+              children={
+                <View className="w-full h-full flex-row items-center justify-center gap-2">
+                  <MaterialIcons
+                    name="delete-sweep"
+                    size={20}
+                    color="#FFFFFF"
+                  />
+                  <Text className="font-interSemiBold text-[14px] text-white">
+                    Limpar tudo
+                  </Text>
+                </View>
+              }
+            />
+          )}
 
           {totalPages > 1 && (
             <Pagination
